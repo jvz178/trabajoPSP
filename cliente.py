28c1e24e5faf2ba7f8388ce3ea5689b1bd8aaa1e
santii243 <72436574+santii243@users.noreply.github.com>
2020-11-25 21:30:40 +0100

    actual

--- cliente.py
+++ cliente.py
@@ -14,6 +14,7 @@ class Cliente:
         if respuesta == "ok":
             self.conectado = True
             print("login con exito")
+        else: print(respuesta)
 
     def logout(self):
         if self.conectado:
